4d4fe0abb44783e0d95a564a7ceba4344e546b5f
LanceZhu <lancezhu2937@gmail.com>
2020-08-23 13:42:59 +0800

    chore: remove unused dependency

--- config/config.default.js
+++ config/config.default.js
@@ -18,27 +18,6 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
-  // mysql
-  config.mysql = {
-    // 单数据库信息配置
-    client: {
-      // host
-      host: 'localhost',
-      // 端口号
-      port: '3306',
-      // 用户名
-      user: 'litemall',
-      // 密码
-      password: 'litemall123456',
-      // 数据库名
-      database: 'litemall',
-    },
-    // 是否加载到 app 上，默认开启
-    app: true,
-    // 是否加载到 agent 上，默认关闭
-    agent: false,
-  };
-
   // sequelize
   config.sequelize = {
     dialect: 'mysql',
--- config/plugin.js
+++ config/plugin.js
@@ -6,10 +6,6 @@ module.exports = {
   // static: {
   //   enable: true,
   // }
-  mysql: {
-    enable: true,
-    package: 'egg-mysql',
-  },
   sequelize: {
     enable: true,
     package: 'egg-sequelize',
